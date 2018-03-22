b39e083bfff60bd7ff8cbb549da8a801099d4d1d
Ervin Ramic <ervin@flexpos.com>
2018-03-22 08:19:44 +0100

    spiderman updated :D

--- src/app/_service/SpiderMan.ts
+++ src/app/_service/SpiderMan.ts
@@ -1,52 +1,62 @@
-// import { Injectable,  } from "@angular/core";
-// import { Headers, Http, RequestOptions, RequestOptionsArgs } from "@angular/http";
-// import "rxjs/add/operator/toPromise";
+import { Injectable } from "@angular/core";
+import { Headers, Http, RequestOptions, RequestOptionsArgs, } from "@angular/http";
+import { Observable } from 'rxjs/Observable';
+import "rxjs/add/operator/toPromise";
 
-// @Injectable()
+ @Injectable()
+ export class WebShooter {
+     constructor(private http: Http) {
+     }
 
-// export class WebShooter {
-    
-//     constructor(private http: Http, defaultOptions: RequestOptions) {
-//     }
+    get(url: string, options?: RequestOptionsArgs): Observable<any> {
+         this.requestInterceptor();
+         return this.http.get(this.getFullUrl(url), this.requestOptions(options))
+            .catch(this.onCatch.bind(this))
+            .do((res: Response) => {}, (error: any) => {})
+            .finally(() => {});
+    }
+    post(url: string, body: any, options?: RequestOptionsArgs): Observable<any> {
+        this.requestInterceptor();
+        return this.http.post(this.getFullUrl(url), body, this.requestOptions(options))
+            .catch(this.onCatch.bind(this))
+            .do((res: Response) => {}, (error: any) => {})
+            .finally(() => {});
+    }
+    put(url: string, body: any, options?: RequestOptionsArgs): Observable<any> {
+        this.requestInterceptor();
+        return this.http.put(this.getFullUrl(url), body, this.requestOptions(options))
+            .catch(this.onCatch.bind(this))
+            .do((res: Response) => {}, (error: any) => {})
+            .finally(() => {});
+    }
+    delete(url: string, options?: RequestOptionsArgs): Observable<any> {
+        this.requestInterceptor();
+        return this.http.delete(this.getFullUrl(url), this.requestOptions(options))
+            .catch(this.onCatch.bind(this))
+            .do((res: Response) => {}, (error: any) => {})
+            .finally(() => {});
+    }
+    private requestInterceptor(): void {
+    }
+    private onCatch(error: any, caught: Observable<any>): Observable<any> {
+        return Observable.of(error);        
+    }
+    private getFullUrl(url: string): string {
+        return '';
+    }
+    private requestOptions(options?: RequestOptionsArgs): RequestOptionsArgs {
+        if (options == null) {
+            options = new RequestOptions();
+        }
 
-//     get(url: string, options?: RequestOptionsArgs): Observable<any> {
+        if (options.headers == null) {
+            const user = localStorage.getItem('user') != "undefined" ? JSON.parse(localStorage.getItem('user')) : null;
+            options.headers = new Headers({
+                'Content-Type': 'application/json',
+                'X-Spree-Token': user && user.spree_api_key
+              });
+        }
 
-//       var accessToken;
-      
-//       return this.http.get('http://localhost:50414/api/_temp', this.getOptions(accessToken)).toPromise() 
-//       .then(response => {
-
-//       });
-//     }
-
-//     login(userName: string, password: string): Promise<_tmpToken> {        
-//         return this.http.post('http://localhost:50414/api/Auth', this.getParams(userName, password), this.getOptions(null))
-//         .toPromise()
-//         .then(response => {
-//                 let token = response.json() as _tmpToken;
-//                 if (token.stateCode == 1) this.unlockAccess(token.accessToken);
-                
-//                 let tokenManager:tknMngr = new tknMngr();
-//                 tokenManager.saveTkn(token);
-                
-//                 return token;
-//             })
-//     }
-//     private unlockAccess(accessToken: string){
-//         return this.http.get('http://localhost:50414/api/_temp', this.getOptions(accessToken)).toPromise() 
-//         .then(response => {
-//         })
-//     }
-//     private getParams(userName : string, password : string){
-//         let params = new URLSearchParams();
-//         params.set('UserName', userName)
-//         params.set('Password', password)
-//         return params.toString();
-//     }
-
-//     private getOptions(accessToken: string){
-//         let headers = new Headers({'Content-Type': 'application/x-www-form-urlencoded'});
-//         if(accessToken) headers.set('Authorization', 'Bearer ' + accessToken);
-//         return  new RequestOptions({ headers: headers });
-//     }
-// }
+        return options;
+    }
+}
